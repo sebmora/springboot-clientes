b69d45ddbcea28c5bec391db3d7ed7798dadc0ce
Sebastian Mora <sebastian.mora@imagemaker.cl>
2017-08-22 11:38:02 -0300

    primer push

---
+++ src/main/java/com/sebmora/springboot/springbootclientes/SpringbootClientesApplication.java
@@ -0,0 +1,12 @@
+package com.sebmora.springboot.springbootclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootClientesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sebmora/springboot/springbootclientes/SpringbootClientesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sebmora.springboot.springbootclientes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootClientesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
